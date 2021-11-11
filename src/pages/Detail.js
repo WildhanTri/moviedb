43850fbefa922a9649f661204828c85b5f8b2b4d
Wildhan Triherlian <triherlian@gmail.com>
2021-11-12 00:17:14 +0700

    error handling & back button

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,24 +1,32 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import MovieService from "../services/MovieService";
 
 import imdbLogo from '../assets/imdb-logo.svg';
 import metacriticLogo from '../assets/metacritic-logo.svg';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 
 const Detail = (props) => {
 
 
   let { imdbID } = useParams();
   const [movie, setMovie] = useState({});
+  const [isMovieLoading, setIsMovieLoading] = useState(true);
+
+  const [movieError, setMovieError] = useState("");
 
   const movieService = new MovieService();
   useEffect(() => {
+    setIsMovieLoading(true)
     movieService.getMoviesDetail(imdbID)
       .then((resolve) => {
         setMovie(resolve);
+        setIsMovieLoading(false);
       })
       .catch((error) => {
-
+        setMovieError(error);
+        setIsMovieLoading(false);
       })
   }, [])
 
@@ -36,110 +44,139 @@ const Detail = (props) => {
   }
 
   return (
-    <div style={styles.container}>
-      <div className="row">
-        <div className="col-sm-4 text-center">
-          <img src={movie.Poster} alt={movie.Title} className="w-100" style={{ cursor: 'pointer' }} data-bs-toggle="modal" data-bs-target="#posterModal"></img>
-        </div>
-        <div className="col-sm-8 text-start" style={styles.movieContent}>
-          <div className="mb-4" style={styles.movieHeader}>
-            <div style={styles.movieTitleWrapper}>
-              <h2 style={styles.movieTitle}>{movie.Title}</h2>
-              <h6 style={styles.movieYear}>{movie.Type} • {movie.Released} • {movie.Runtime}</h6>
-            </div>
-            <div style={styles.movieClassificationWrapper}>
-              <div style={styles.movieClassificationContentWrapper}>
-                {movie.Rated}
-              </div>
+    <div>
+      <div style={styles.container}>
+        {
+          isMovieLoading &&
+          <div className="w-100 h-100 d-flex align-items-center justify-content-center pt-5 pb-5">
+            <div className="spinner-border text-primary" role="status">
+              <span className="visually-hidden">Loading...</span>
             </div>
           </div>
-          <div className="mb-4" style={styles.movieDescriptionWrapper}>
-            <div className="mb-4 text-start">
-              {
-                movie.Genre != null && movie.Genre.split(',').map((g, index) => {
-                  return (
-                    <span key={index} className="badge bg-secondary me-2">{g}</span>
-                  )
-                })
-              }
-            </div>
-            <p className="mb-4">
-              {movie.Plot}
-            </p>
-          </div>
+        }
 
-          <div className="mb-4" style={styles.movieDescriptionWrapper}>
-            <p className="mb-1">
-              Directed By : {
-                movie.Director != null && movie.Director.split(',').map((value, index) => {
-                  return (
-                    <span key={index}>{(index > 0 ? '•' : '')} <a href={"https://www.google.com/search?q=" + value} target="_blank">{value}</a></span>
-                  )
-                })
-              }
-            </p>
-            <p className="mb-1">
-              Writed By : {
-                movie.Writer != null && movie.Writer.split(',').map((value, index) => {
-                  return (
-                    <span key={index}>{(index > 0 ? ' • ' : '')} <a href={"https://www.google.com/search?q=" + value} target="_blank">{value}</a></span>
-                  )
-                })
-              }
-            </p>
-            <p className="mb-1">
-              Actors : {
-                movie.Actors != null && movie.Actors.split(',').map((value, index) => {
-                  return (
-                    <span key={index}>{(index > 0 ? ' • ' : '')} <a href={"https://www.google.com/search?q=" + value} target="_blank">{value}</a></span>
-                  )
-                })
-              }
-            </p>
-          </div>
-
-          <div className="mb-4" style={styles.movieDescriptionWrapper}>
-            <h5 className="fw-bold mb-5">Ratings</h5>
+        {
+          !isMovieLoading && movieError === "" &&
+          <div>
             <div className="row">
-              <div className="col-sm-6 text-center mb-5">
-                <div className="d-flex justify-content-center mb-3">
-                  <div style={Object.assign({}, styles.movieRatingWrapper, { background: ratingColor(movie.Metascore, 100) })}>
-                    {movie.Metascore}
-                  </div>
-                </div>
-                <div>
-                  <img src={imdbLogo} alt="imdb" style={{ width: 64 }} ></img>
-                </div>
+              <div className="col-sm-4 text-center">
+                <img src={movie.Poster} alt={movie.Title} className="w-100" style={{ cursor: 'pointer' }} data-bs-toggle="modal" data-bs-target="#posterModal"></img>
               </div>
-              <div className="col-sm-6 text-center mb-5">
-                <div className="d-flex justify-content-center mb-3">
-                  <div style={Object.assign({}, styles.movieRatingWrapper, { background: ratingColor(movie.imdbRating, 10) })}>
-                    {movie.imdbRating}
-                  </div>
-                </div>
-                <div>
-                  <img src={metacriticLogo} alt="imdb" style={{ width: 128 }}></img>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+              <div className="col-sm-8 text-start" style={styles.movieContent}>
 
-      <div className="modal fade" id="posterModal" tabindex="-1" aria-labelledby="posterModalLabel" aria-hidden="true">
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <Link to="/movie">
+                  <button className="btn btn-primary mb-4">
+                    <FontAwesomeIcon icon={faChevronLeft} />
+                    <span className="ms-2">Back</span>
+                  </button>
+                </Link>
+                <div className="mb-4" style={styles.movieHeader}>
+                  <div style={styles.movieTitleWrapper}>
+                    <h2 style={styles.movieTitle}>{movie.Title}</h2>
+                    <h6 style={styles.movieYear}>{movie.Type} • {movie.Released} • {movie.Runtime}</h6>
+                  </div>
+                  <div style={styles.movieClassificationWrapper}>
+                    <div style={styles.movieClassificationContentWrapper}>
+                      {movie.Rated}
+                    </div>
+                  </div>
+                </div>
+                <div className="mb-4" style={styles.movieDescriptionWrapper}>
+                  <div className="mb-4 text-start">
+                    {
+                      movie.Genre != null && movie.Genre.split(',').map((g, index) => {
+                        return (
+                          <span key={index} className="badge bg-secondary me-2">{g}</span>
+                        )
+                      })
+                    }
+                  </div>
+                  <p className="mb-4">
+                    {movie.Plot}
+                  </p>
+                </div>
+
+                <div className="mb-4" style={styles.movieDescriptionWrapper}>
+                  <p className="mb-1">
+                    Directed By : {
+                      movie.Director != null && movie.Director.split(',').map((value, index) => {
+                        return (
+                          <span key={index}>{(index > 0 ? '•' : '')} <a href={"https://www.google.com/search?q=" + value} target="_blank">{value}</a></span>
+                        )
+                      })
+                    }
+                  </p>
+                  <p className="mb-1">
+                    Writed By : {
+                      movie.Writer != null && movie.Writer.split(',').map((value, index) => {
+                        return (
+                          <span key={index}>{(index > 0 ? ' • ' : '')} <a href={"https://www.google.com/search?q=" + value} target="_blank">{value}</a></span>
+                        )
+                      })
+                    }
+                  </p>
+                  <p className="mb-1">
+                    Actors : {
+                      movie.Actors != null && movie.Actors.split(',').map((value, index) => {
+                        return (
+                          <span key={index}>{(index > 0 ? ' • ' : '')} <a href={"https://www.google.com/search?q=" + value} target="_blank">{value}</a></span>
+                        )
+                      })
+                    }
+                  </p>
+                </div>
+
+                <div className="mb-4" style={styles.movieDescriptionWrapper}>
+                  <h5 className="fw-bold mb-5">Ratings</h5>
+                  <div className="row">
+                    <div className="col-sm-6 text-center mb-5">
+                      <div className="d-flex justify-content-center mb-3">
+                        <div style={Object.assign({}, styles.movieRatingWrapper, { background: ratingColor(movie.Metascore, 100) })}>
+                          {movie.Metascore}
+                        </div>
+                      </div>
+                      <div>
+                        <img src={imdbLogo} alt="imdb" style={{ width: 64 }} ></img>
+                      </div>
+                    </div>
+                    <div className="col-sm-6 text-center mb-5">
+                      <div className="d-flex justify-content-center mb-3">
+                        <div style={Object.assign({}, styles.movieRatingWrapper, { background: ratingColor(movie.imdbRating, 10) })}>
+                          {movie.imdbRating}
+                        </div>
+                      </div>
+                      <div>
+                        <img src={metacriticLogo} alt="imdb" style={{ width: 128 }}></img>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="modal-body">
-              <img src={movie.Poster} alt={movie.Title} className="w-100"></img>
-            </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+
+            <div className="modal fade" id="posterModal" tabindex="-1" aria-labelledby="posterModalLabel" aria-hidden="true">
+              <div className="modal-dialog">
+                <div className="modal-content">
+                  <div className="modal-header">
+                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div className="modal-body">
+                    <img src={movie.Poster} alt={movie.Title} className="w-100"></img>
+                  </div>
+                  <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
+        }
+        {
+          !isMovieLoading && movieError !== "" &&
+          <div className="w-100 h-100 d-flex align-items-center justify-content-center pt-5 pb-5">
+            {movieError}
+          </div>
+        }
       </div>
     </div>
   )

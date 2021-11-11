55612af8c514d8fd5e314034d27a8ee8abc18c47
Wildhan Triherlian <triherlian@gmail.com>
2021-11-11 19:34:50 +0700

    Add Detail Component

---
+++ pages/Detail.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from "react";
+
+const Detail = () => {
+    
+  useEffect(() => {
+    
+  }, [])
+
+  return (
+    <div style={styles.container}>
+        This is detail
+    </div>
+  )
+}
+
+const styles = {
+  container: {
+  }
+}
+
+export default Detail;
